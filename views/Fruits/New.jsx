35ba4a5c17728ef994c56dfbaeceb984e08d180a
Mindy Miller <mmillerks@gmail.com>
2022-02-16 18:19:42 -0600

    Added gitignore and installed dotenv

--- views/Fruits/New.jsx
+++ views/Fruits/New.jsx
@@ -6,6 +6,9 @@ class New extends React.Component {
         <div>
             <h1>Hi, this is the New Page.</h1>
             { /* This is how you make a comment. */}
+            <nav>
+            <a href="/fruits/new">Back to Fruits Index Page</a>
+            </nav>
             <form action="/fruits" method="POST">
                 Name Here: <input name="name" type="text"/><br/>
                 Enter Color: <input name="color" type="text"/><br/>
--- views/Vegetables/Show.jsx
+++ views/Vegetables/Show.jsx
@@ -8,7 +8,7 @@ class Show extends React.Component {
       <h1> Show Page </h1>
       <a href="/vegetables">Go back to Home Page</a>
       <p>The {vegetables.name} is {vegetables.color}</p>
-      <p>{vegetables.readyToEat? 'Its is ready to eat' : 'It is not ready to eat... Cant touch this' }</p>
+      <p>{vegetables.readyToEat? 'It is ready to eat' : 'It is not ready to eat... Cant touch this' }</p>
       </div>
       );
      }
